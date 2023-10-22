1439bbfc3250015466c428c7b8eced11018280e3
joerivervoort1 <joerivervoort@hotmail.com>
2023-10-22 21:39:43 +0200

    finished

--- Gyprocplaatsen.py
+++ Gyprocplaatsen.py
@@ -1,1 +1,10 @@
-print("test1")
+import math
+
+# functie om de plaatsingskost te berekenen
+def plaatsingskost(opp,dikte):
+    if dikte == 6:
+        platen = float(opp / 1,44)
+        prijs = math.ceil(platen) * 6.2
+        return prijs
+
+print(plaatsingskost(20,6))
